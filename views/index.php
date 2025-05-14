cc3d50e4b38a51103ff6861ad5379f38d2d104a9
boudewijn010 <Boudewijnvandamme@gmail.com>
2025-05-14 08:43:44 +0200

    Add initial HTML structure and CSS file for the web page
    - Created index.php with basic HTML layout including header and navigation.
    - Added links for Home, Shop, About Us, and Contact in the navigation menu.
    - Included a placeholder CSS file (styles.css) for future styling.

---
+++ views/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>My Web Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <img src="img\webshop-icon.jpg" alt="Logo" class="logo">
+        <img src="img\add-to-card.webp" alt="Add to Cart" class="add-to-cart">
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="shop.php">shop</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+
+</html>
