b21011865cd3a23c945eafdb6450bb0b209e2ceb
boudewijn010 <Boudewijnvandamme@gmail.com>
2025-05-15 12:26:42 +0200

    Remove unused shop model and related methods from HomepageController

--- controllers/homepage.php
+++ controllers/homepage.php
@@ -4,18 +4,7 @@ include_once __DIR__ . '/../views/homepage.php';
 
 class HomepageController
 {
-    private $shopModel;
 
-    public function __construct()
-    {
-        $this->shopModel = new Shop();
-    }
-
-    public function index()
-    {
-        $products = $this->shopModel->getAllProducts();
-        include __DIR__ . '/../views/homepage.php';
-    }
 }
 
 ?>
