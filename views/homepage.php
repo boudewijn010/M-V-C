39cedb6cb5e2f1840cb7a65f1e421682c7f0c1eb
boudewijn010 <Boudewijnvandamme@gmail.com>
2025-05-19 12:39:03 +0200

    Fix stylesheet path and update header inclusion in homepage.php

--- views/homepage.php
+++ views/homepage.php
@@ -2,14 +2,18 @@
 
 <head>
     <title>homepage</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="/views/styles.css">
 </head>
 
 <body>
     <?php
-    include_once __DIR__ . '/../views/header.php';
+    require_once __DIR__ . '/../views/header.php';
     ?>
 
+    <main>
+        <h1>Welkom op de homepage!</h1>
+        <p>Dit is de startpagina van je webshop.</p>
+    </main>
 </body>
 
 </html>
